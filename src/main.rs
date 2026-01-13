d8c022e59f5171d1d8658328166e154ba6106b16
Icy26 <Icy26@>
2026-01-13 09:34:43 -0500

    Add detection for empty vs non-empty backup folders and handling for both cases

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+use std::fmt::Write;
+use std::path::PathBuf;
+use std::ffi::OsString;
+
 fn main() {
 	//temp, will be moved to config later
 	let world_path = "testworld";
@@ -5,8 +12,24 @@ fn main() {
 	//let backup_frequency; //add flag to force backup
 
 	//get the most recent backup
-	//get the current save files
-	//check timestamps for changes
-	//generate manifest
-	//copy changed files
+	let mut backup_folders = fs::read_dir(backup_dir)
+		.expect("backup dir inaccessible")
+		.map(|folder| {
+			folder
+				.expect("backup inacessible")
+				.file_name()
+		})
+		.collect::<Vec<OsString>>();
+
+	//check if previous backup exists
+	match backup_folders.is_empty() {
+		true => full_backup(world_path, backup_dir), //if no previous backups, perform full backup
+		false => iterative_backup(world_path, backup_dir) // if there are previous backups, perform iterative backup
+	}
+}
+
+fn full_backup(world_path: &str, backup_dir: &str) -> () {
+}
+
+fn iterative_backup(world_path: &str, backup_dir: &str) -> () {
 }
