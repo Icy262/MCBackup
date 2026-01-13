b8c59ffd4e0485a91dcb4492b0cda4910bd541a5
Icy26 <Icy26@>
2026-01-13 09:47:47 -0500

    Improve empty backup directory detection efficiency

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,11 @@ fn main() {
 	let backup_dir = "testbackup";
 	//let backup_frequency; //add flag to force backup
 
-	//get the most recent backup
-	let mut backup_folders = fs::read_dir(backup_dir)
-		.expect("backup dir inaccessible")
-		.map(|folder| {
-			folder
-				.expect("backup inacessible")
-				.file_name()
-		})
-		.collect::<Vec<OsString>>();
-
 	//check if previous backup exists
-	match backup_folders.is_empty() {
+	match fs::read_dir(backup_dir)
+		.expect("backup dir could not be read")
+		.next()
+		.is_none() {
 		true => full_backup(world_path, backup_dir), //if no previous backups, perform full backup
 		false => iterative_backup(world_path, backup_dir) // if there are previous backups, perform iterative backup
 	}
