980cafc0a4ea7dba9ae7fb1a8af87e44c7e4ffa4
Icy26 <Icy26@>
2026-01-13 08:03:37 -0500

    Add project template

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+	//temp, will be moved to config later
+	let world_path = "testworld";
+	let backup_dir = "testbackup";
+	//let backup_frequency; //add flag to force backup
+
+	//get the most recent backup
+	//get the current save files
+	//check timestamps for changes
+	//generate manifest
+	//copy changed files
+}
