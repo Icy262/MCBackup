480fd37d8b1cb5ed73762a8ed4e3926bb47f6fd8
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-01-13 13:38:00 -0500

    Implement directory creation for iterative backups and enumerate the regions to backup. Also cargo fmt run.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,76 @@
-use std::fs;
+use std::collections::btree_set::Difference;
 use std::ffi::OsString;
+use std::fs::{self, Metadata};
 use time::OffsetDateTime;
 use time::macros::format_description;
 
+const FORMAT: &[time::format_description::FormatItem<'static>] =
+	format_description!("[year]-[month]-[day]T[hour]-[minute]");
+
 fn main() {
-	const FORMAT: &[time::format_description::FormatItem<'static>] = format_description!("[year]-[month]-[day]T[hour]-[minute]");
 	//temp, will be moved to config later
 	let world_path = "testworld";
 	let backup_dir = "testbackup";
+	let dims = vec!["region", "DIM1/region", "DIM-1/region"]; //vanilla minecraft uses these three directories. add support for additional directories for modded worlds
 	//let backup_frequency; //add flag to force backup
 
 	//check if previous backup exists
 	match fs::read_dir(backup_dir)
 		.expect("backup dir could not be read")
 		.next()
-		.is_none() {
-		true => full_backup(world_path, backup_dir), //if no previous backups, perform full backup
-		false => iterative_backup(world_path, backup_dir) // if there are previous backups, perform iterative backup
+		.is_none()
+	{
+		true => full_backup(world_path, backup_dir, dims), //if no previous backups, perform full backup
+		false => iterative_backup(world_path, backup_dir, dims), // if there are previous backups, perform iterative backup
 	}
 }
 
-fn full_backup(world_path: &str, backup_dir: &str) -> () {
-}
+fn full_backup(world_path: &str, backup_dir: &str, dims: Vec<&str>) -> () {}
 
-fn iterative_backup(world_path: &str, backup_dir: &str) -> () {
+fn iterative_backup(world_path: &str, backup_dir: &str, dims: Vec<&str>) -> () {
 	//get a list of old backups
-	let mut backup_folders = fs::read_dir(backup_dir)
+	let mut backups = fs::read_dir(backup_dir)
 		.expect("backup dir inaccessible")
-		.map(|folder| {
-			folder
-				.expect("backup inacessible")
-				.file_name()
-		})
+		.map(|folder| folder.expect("backup inacessible").file_name())
 		.collect::<Vec<OsString>>();
 
 	//find most recent backup by sorting, reversing, and getting the first element
-	backup_folders.sort();
-	backup_folders.reverse();
-	let most_recent_backup = backup_folders
-		.get(0)
-		.expect("backup dir empty")
-		.clone();
-	print!("{:?}", most_recent_backup);
+	backups.sort();
+	backups.reverse();
+	let most_recent_backup = backups.get(0).expect("backup dir empty").clone();
 
-	//get the current save files
-	//check timestamps for changes
-	//generate manifest
-	//copy changed files
+	//create directory to store new backup
+	let new_backup = format!(
+		"{}/{}",
+		backups,
+		OffsetDateTime::now_utc()
+			.format(FORMAT)
+			.expect("current time to timestamp conversion failed")
+	);
+	fs::create_dir_all(new_backup);
 
+	//for each dimension,
+	for dim in dims {
+		let this_dim_backup = format!("{}/{}", new_backup, dim);
+
+		//create new directory in backup folder to store this dimension
+		fs::create_dir(this_dim_backup);
+
+		//get the names for the region files for this dimension
+		let mut region_files = fs::read_dir(format!("{}/{}", world_path, this_dim_backup))
+			.expect("world files unreadable");
+
+		//collect read dir into Vec of file names
+		let mut region_files = region_files
+			.map(|region| region.expect("region file could not be read").file_name())
+			.collect::<Vec<OsString>>();
+
+		//sort files by name
+		region_files.sort();
+
+		//check timestamps for changes
+
+		//generate manifest
+		//copy changed files
+	}
 }
