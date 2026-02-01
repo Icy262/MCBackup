4b30fb94b51dcd6d765c37a0ebe676a21ed2d7f7
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-02-01 13:31:41 -0500

    Add remove backup functionality

--- src/remove.rs
+++ src/remove.rs
@@ -1,14 +1,31 @@
-use crate::util::backup;
-use std::path::PathBuf;
+use rusqlite::{Connection, params};
 
-pub(crate) fn remove(path_to_backup_dir: &PathBuf, timestamp: String) {
-	//get the path to the next backup
-	let path_to_next_backup = backup::get_next(path_to_backup_dir, &timestamp);
+use crate::util::{self, backup};
+use std::path::PathBuf;
+use std::fs;
+
+pub(crate) fn remove(path_to_backups_dir: &PathBuf, timestamp: &String, database_connection: &Connection) {
+	//get the timestamp of the next backup
+	let next_backup_timestamp = backup::get_next(database_connection, &timestamp).expect("Should be a next backup");
 
 	//copy any files from this backup to the next one
-	//update the next backup's manifest to remove any files we copied
-}
+	util::dir_operation::copy(&path_to_backups_dir.join(&timestamp), &path_to_backups_dir.join(&next_backup_timestamp));
 
-pub(crate) fn remove_range(timestamp: String) {
-	//call remove repeatedly for all timestamps in the range
+	//update the next backup's manifest to update the timestamps of moved files
+	database_connection.execute(
+		format!("UPDATE \"{}\"
+		SET timestamp = ?1
+		WHERE timestamp = ?2
+		", next_backup_timestamp).as_str(),
+		params![&next_backup_timestamp, &timestamp])
+		.expect("Updating timestamps failed");
+
+	database_connection.execute(
+		format!("DROP TABLE \"{}\"",
+		timestamp).as_str(),
+		[])
+		.expect("Should be able to drop deleted backup table");
+
+	//remove directory
+	fs::remove_dir_all(path_to_backups_dir.join(timestamp)).expect("Should be able to delete old backup");
 }
