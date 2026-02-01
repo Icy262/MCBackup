dc57cbd723901d0fbe59eda56710d7924a6661bf
Icy26 <Icy26@>
2026-02-01 13:42:40 -0500

    Remove unused functions

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,5 @@
 use std::path::PathBuf;
 
-pub(crate) fn get_file_name_as_str(path_to_file: &PathBuf) -> &str {
-	path_to_file
-		.file_name()
-		.expect("Should be able to get the file name of the file referenced in the path")
-		.to_str()
-		.expect("Should be able to convert OsString to String")
-}
-
 pub(crate) fn trim_path(path: &PathBuf, level: &PathBuf) -> PathBuf {
 	//to be faster on batch operations, level should be cannonicalized before passing. Path should be a child of level.
 	path.canonicalize()
@@ -35,13 +27,6 @@ pub(crate) mod timestamp {
 			.assume_offset(
 				UtcOffset::current_local_offset().expect("Should be able to get time zone"),
 			)
-	}
-
-	#[allow(non_snake_case)]
-	pub(crate) fn to_String(timestamp: &OffsetDateTime) -> String {
-		timestamp
-			.format(FORMAT)
-			.expect("Should be able to convert timestamp to String")
 	}
 
 	pub(crate) fn get_timestamp(file: &PathBuf) -> OffsetDateTime {
@@ -119,7 +104,6 @@ use crate::util::trim_path;
 
 pub(crate) mod backup {
 	use rusqlite::{Connection, OptionalExtension};
-	use crate::util::dir_operation;
 	use std::fs::{self, create_dir_all};
 	use std::path::PathBuf;
 
@@ -161,31 +145,6 @@ pub(crate) mod backup {
 		)
 		.optional()
 		.expect("Should be able to get table with most recent timestamp")
-	}
-
-	pub(crate) fn get_prev(database_connection: &Connection, timestamp: &String) -> Option<String> {
-		database_connection.query_row(
-			"SELECT name
-			FROM sqlite_schema
-			WHERE type = 'table' AND name > ?1
-			ORDER BY name DESC
-			LIMIT 1;",
-			[timestamp],
-			|row| row.get::<_, String>("name")
-		)
-		.optional()
-		.expect("Should be able to get table with most recent timestamp")
-	}
-
-	pub(crate) fn get_all_backups_sorted(path_to_backup_dir: &PathBuf) -> Vec<PathBuf> {
-		//get the paths to the backups in the backup directory
-		let mut path_to_backups = dir_operation::get_files(path_to_backup_dir);
-
-		//find most recent backup by sorting, reversing, and getting the first element
-		path_to_backups.sort();
-		path_to_backups.reverse();
-
-		return path_to_backups;
 	}
 
 	pub(crate) fn init(path_to_backup: &PathBuf, files: &Vec<PathBuf>, current_time: &String, database_connection: &Connection) -> () {
